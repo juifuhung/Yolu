7d1d5579901ab80383732d19dcd92f369da03788
Ralph Hung <juifuhung@gmail.com>
2022-06-20 21:31:09 +0800

    displayed InfoWindow when clicking icon

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -1,9 +1,22 @@
 import React, { useMemo, useState, useEffect } from "react";
 import "../CSS/Map.css";
 import { v4 as uuidv4 } from "uuid";
-import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
+import { FaHeart, FaStar } from "react-icons/fa";
+import {
+  GoogleMap,
+  useLoadScript,
+  Marker,
+  InfoWindow,
+} from "@react-google-maps/api";
 import { initializeApp } from "firebase/app";
-import { getFirestore, getDocs, collection } from "firebase/firestore";
+import {
+  getFirestore,
+  getDocs,
+  collection,
+  doc,
+  setDoc,
+  deleteDoc,
+} from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -22,9 +35,15 @@ const Map = () => {
   const center = useMemo(() => ({ lat: 66.533688, lng: 25.75218 }), []);
 
   const [array, setArray] = useState([]);
+  const [favorites, setFavorites] = useState([]);
+  const [selected, setSelected] = useState(null);
 
   useEffect(() => {
     getData();
+  }, []);
+
+  useEffect(() => {
+    getFavorites();
   }, []);
 
   const getData = async () => {
@@ -34,6 +53,34 @@ const Map = () => {
       array.push(doc.data());
     });
     setArray(array);
+  };
+
+  const getFavorites = async () => {
+    let favoritesArray = [];
+    const querySnapshot = await getDocs(collection(db, "Favorites"));
+    querySnapshot.forEach((doc) => {
+      favoritesArray.push(doc.data());
+    });
+    setFavorites(favoritesArray);
+  };
+
+  const deleteHandler = async (id) => {
+    await deleteDoc(doc(db, "Favorites", `${id}`));
+  };
+
+  const addToFavorite = async (obj) => {
+    try {
+      await setDoc(doc(db, "Favorites", `${obj.id}`), {
+        id: obj.id,
+        title: obj.title,
+        category: obj.category,
+        description: obj.description,
+        photo: obj.img,
+        created_time: new Date(),
+      });
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
   };
 
   const { isLoaded } = useLoadScript({
@@ -56,8 +103,46 @@ const Map = () => {
             key={uuidv4()}
             icon={location.icon}
             position={{ lat: location.lat, lng: location.lng }}
+            onClick={() => {
+              setSelected(location);
+            }}
           />
         ))}
+        {selected && (
+          <InfoWindow
+            position={{ lat: selected.lat, lng: selected.lng }}
+            onCloseClick={() => setSelected(null)}
+          >
+            <div>
+              <h1>{selected.title}</h1>
+              <p>{selected.description}</p>
+              {favorites.find((element) => element.id === selected.id) ? (
+                <FaStar
+                  onClick={() => {
+                    deleteHandler(selected.id);
+                    getFavorites();
+                    alert(`removed ${selected.title} from favorite list`);
+                  }}
+                />
+              ) : (
+                <FaHeart
+                  onClick={() => {
+                    addToFavorite({
+                      id: selected.id,
+                      category: selected.category,
+                      title: selected.title,
+                      description: selected.description,
+                      img: selected.image,
+                    });
+                    getFavorites();
+                    alert(`added ${selected.title} to favorite list`);
+                  }}
+                />
+              )}
+              <img src={selected.image} alt="" />
+            </div>
+          </InfoWindow>
+        )}
       </GoogleMap>
       <p>map</p>
     </div>
