905ed906f2d3c2e409a2b39886396c788f1e5952
Ralph Hung <juifuhung@gmail.com>
2022-06-20 21:10:29 +0800

    initialized Google Map

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useMemo } from "react";
+import "../CSS/Map.css";
+import { GoogleMap, useLoadScript } from "@react-google-maps/api";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+};
+
+initializeApp(firebaseConfig);
+getFirestore();
 
 const Map = () => {
-  return <div>Map</div>;
+  const center = useMemo(() => ({ lat: 66.533688, lng: 25.75218 }), []);
+
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+  });
+
+  if (!isLoaded) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div style={{ display: "flex" }}>
+      <GoogleMap
+        zoom={12}
+        center={center}
+        mapContainerClassName="map-container"
+      />
+      <p>map</p>
+    </div>
+  );
 };
 
 export default Map;
