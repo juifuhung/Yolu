2a922c6f81981f3765d330c28d9ce686570b446c
Ralph Hung <juifuhung@gmail.com>
2022-07-04 10:19:17 +0800

    added image when there is no spot on favorites page

--- src/utils/Firebase.js
+++ src/utils/Firebase.js
@@ -48,7 +48,7 @@ export const useAuth = () => {
     if (user) {
       // User is signed in, see docs for a list of available properties
       // https://firebase.google.com/docs/reference/js/firebase.User
-      const uid = user.uid;
+      // const uid = user.uid;
       // ...
     } else {
       // User is signed out
