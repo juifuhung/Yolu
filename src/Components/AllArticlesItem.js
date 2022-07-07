b9153b438a0752446ae9f4969791e05cd593bb7b
Ralph Hung <juifuhung@gmail.com>
2022-07-07 14:26:01 +0800

    finnished basic layout of all article page

--- src/Components/AllArticlesItem.js
+++ src/Components/AllArticlesItem.js
@@ -3,22 +3,49 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const Container = styled(Link)`
-  width: 500px;
-  height: 80px;
-  border: solid green 1px;
-  background-color: pink;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  text-decoration: none;
+  color: black;
+  width: 170px;
+  height: 212px;
+  margin: 0.5rem 1.5rem 0.5rem 0;
+  border: solid red 1px;
 `;
 
-const Title = styled.div`
+const ImageContainer = styled.div`
+  border: solid yellow 1px;
   width: 100%;
+  height: 170px;
+  border-radius: 2rem;
+  background-image: url("${(props) => props.image}");
+  background-size: 200%;
+  background-position: center;
+  background-repeat: no-repeat;
+  transition: all 0.3s ease-in-out;
+
+  &:hover {
+    background-size: 250%;
+  }
+`;
+
+const Title = styled.p`
+  margin: 0;
+  font-size: 1.1rem;
+  font-weight: 600;
+  border: solid blue 1px;
+  width: auto;
   height: 30px;
 `;
 
 const AllArticlesItem = (item) => {
-  console.log(item);
+  console.log(item.image);
   return (
     <>
       <Container to={`./${item.title}`} target="_blank">
+        <ImageContainer image={item.image} />
         <Title>{item.title}</Title>
       </Container>
     </>
--- src/pages/AllArticles.js
+++ src/pages/AllArticles.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { FaEdit } from "react-icons/fa";
 import { initializeApp } from "firebase/app";
 import { getFirestore, getDocs, collection } from "firebase/firestore";
 import AllArticlesItem from "../components/AllArticlesItem";
@@ -17,22 +18,77 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_APP_ID,
 };
 
-const CategoryBlock = styled.div`
+const BodyContainer = styled.div`
   width: 100%;
-  height: 200px;
-  border: solid black 1rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border: solid red 0.5px;
+`;
+
+const TitleContainer = styled.div`
+  height: 100px;
+  width: 85%;
+  background-color: pink;
+  display: flex;
   margin: 2rem 0;
+  justify-content: space-between;
+  align-items: end;
+  border: solid blue 1px;
+`;
+
+const Title = styled.div`
+  width: 80%;
+  border: solid green 1px;
+  font-size: 3.5rem;
+  font-weight: 800;
+`;
+
+const EditSection = styled(Link)`
+  display: flex;
+  align-items: end;
+  border: solid 1px brown;
+  height: auto;
+  width: auto;
+  text-decoration: none;
+`;
+
+const EditIcon = styled(FaEdit)`
+  width: 50px;
+  height: 50px;
+  color: #616161;
+`;
+
+const EditWords = styled.h3`
+  margin: 0 0 0 0.5rem;
+  font-size: 2rem;
+  color: #616161;
+`;
+
+const CategoryBlock = styled.div`
+  width: 85%;
+  height: 350px;
+  border: solid black 1px;
+  margin: 1rem;
+`;
+
+const CategoryRedLine = styled.div`
+  width: 100%;
+  height: 18px;
+  margin: 0.7rem 0;
+  background-color: #ff0000;
+`;
+
+const ItemSection = styled.div`
+  width: 100%;
+  height: 70%;
+  display: flex;
+  border: solid green 1px;
 `;
 
 const Label = styled.h2`
   margin: 0;
-  font-size: 1.5rem;
-`;
-
-const NewPostLink = styled(Link)`
-  width: 500px;
-  height: 200px;
-  background-color: blue;
+  font-size: 2rem;
 `;
 
 initializeApp(firebaseConfig);
@@ -61,56 +117,105 @@ const AllArticles = () => {
   return (
     <>
       <Header />
-      AllArticles
-      <CategoryBlock>
-        <Label>博物館</Label>
-        {allSpots
-          .filter((item) => item.category === "博物館")
-          .map((item) => (
-            <AllArticlesItem key={item.title} title={item.title} />
-          ))}
-      </CategoryBlock>
-      <CategoryBlock>
-        <Label>自然景觀</Label>
-        {allSpots
-          .filter((item) => item.category === "自然景觀")
-          .map((item) => (
-            <AllArticlesItem key={item.title} title={item.title} />
-          ))}
-      </CategoryBlock>
-      <CategoryBlock>
-        <Label>餐廳</Label>
-        {allSpots
-          .filter((item) => item.category === "餐廳")
-          .map((item) => (
-            <AllArticlesItem key={item.title} title={item.title} />
-          ))}
-      </CategoryBlock>
-      <CategoryBlock>
-        <Label>聖誕主題</Label>
-        {allSpots
-          .filter((item) => item.category === "聖誕主題")
-          .map((item) => (
-            <AllArticlesItem key={item.title} title={item.title} />
-          ))}
-      </CategoryBlock>
-      <CategoryBlock>
-        <Label>購物</Label>
-        {allSpots
-          .filter((item) => item.category === "購物")
-          .map((item) => (
-            <AllArticlesItem key={item.title} title={item.title} />
-          ))}
-      </CategoryBlock>
-      <CategoryBlock>
-        <Label>交通</Label>
-        {allSpots
-          .filter((item) => item.category === "交通")
-          .map((item) => (
-            <AllArticlesItem key={item.title} title={item.title} />
-          ))}
-      </CategoryBlock>
-      <NewPostLink to="/new-post">寫新遊記</NewPostLink>
+      <BodyContainer>
+        <TitleContainer>
+          <Title>所有文章</Title>
+          <EditSection to={"/new-post"}>
+            <EditIcon />
+            <EditWords>發表文章</EditWords>
+          </EditSection>
+        </TitleContainer>
+        <CategoryBlock>
+          <Label>博物館</Label>
+          <CategoryRedLine />
+          <ItemSection>
+            {allSpots
+              .filter((item) => item.category === "博物館")
+              .map((item) => (
+                <AllArticlesItem
+                  key={item.title}
+                  title={item.title}
+                  image={item.image}
+                />
+              ))}
+          </ItemSection>
+        </CategoryBlock>
+        <CategoryBlock>
+          <Label>自然景觀</Label>
+          <CategoryRedLine />
+          <ItemSection>
+            {allSpots
+              .filter((item) => item.category === "自然景觀")
+              .map((item) => (
+                <AllArticlesItem
+                  key={item.title}
+                  title={item.title}
+                  image={item.image}
+                />
+              ))}
+          </ItemSection>
+        </CategoryBlock>
+        <CategoryBlock>
+          <Label>餐廳</Label>
+          <CategoryRedLine />
+          <ItemSection>
+            {allSpots
+              .filter((item) => item.category === "餐廳")
+              .map((item) => (
+                <AllArticlesItem
+                  key={item.title}
+                  title={item.title}
+                  image={item.image}
+                />
+              ))}
+          </ItemSection>
+        </CategoryBlock>
+        <CategoryBlock>
+          <Label>聖誕主題</Label>
+          <CategoryRedLine />
+          <ItemSection>
+            {allSpots
+              .filter((item) => item.category === "聖誕主題")
+              .map((item) => (
+                <AllArticlesItem
+                  key={item.title}
+                  title={item.title}
+                  image={item.image}
+                />
+              ))}
+          </ItemSection>
+        </CategoryBlock>
+        <CategoryBlock>
+          <Label>購物</Label>
+          <CategoryRedLine />
+          <ItemSection>
+            {allSpots
+              .filter((item) => item.category === "購物")
+              .map((item) => (
+                <AllArticlesItem
+                  key={item.title}
+                  title={item.title}
+                  image={item.image}
+                />
+              ))}
+          </ItemSection>
+        </CategoryBlock>
+        <CategoryBlock>
+          <Label>交通</Label>
+          <CategoryRedLine />
+          <ItemSection>
+            {allSpots
+              .filter((item) => item.category === "交通")
+              .map((item) => (
+                <AllArticlesItem
+                  key={item.title}
+                  title={item.title}
+                  image={item.image}
+                />
+              ))}
+          </ItemSection>
+        </CategoryBlock>
+      </BodyContainer>
       <Footer />
     </>
   );
